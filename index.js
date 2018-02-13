98dfe2a670332577759919a2cbc1c8f0772f3c2c
Mihai Childesco <mihai.childesco@gmail.com>
2018-02-13 22:59:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's `+ days +  ` days until ${holiday}`;
+  return `It's `+ days +  ` days until ${holiday}!`;
 }
 
 
